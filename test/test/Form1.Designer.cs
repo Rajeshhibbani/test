0046727654edc77eb808347809c981a40db8203e
Rajeshhibbani <rajesh.nalina18@gmail.com>
2024-10-25 02:05:09 +0530

    Add files via upload

---
+++ test/test/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Close_button = new System.Windows.Forms.Button();
+            this.Terminate_current_instance = new System.Windows.Forms.Button();
+            this.New_instance = new System.Windows.Forms.Button();
+            this.Terminate_all_process = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AccessibleName = "";
+            this.panel1.AutoSize = true;
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.Close_button);
+            this.panel1.Location = new System.Drawing.Point(-1, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(804, 30);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // Close_button
+            // 
+            this.Close_button.AutoSize = true;
+            this.Close_button.Location = new System.Drawing.Point(761, 1);
+            this.Close_button.Name = "Close_button";
+            this.Close_button.Size = new System.Drawing.Size(40, 26);
+            this.Close_button.TabIndex = 0;
+            this.Close_button.Text = "X";
+            this.Close_button.UseVisualStyleBackColor = true;
+            this.Close_button.Click += new System.EventHandler(this.Close_button_Click);
+            // 
+            // Terminate_current_instance
+            // 
+            this.Terminate_current_instance.AutoSize = true;
+            this.Terminate_current_instance.Location = new System.Drawing.Point(47, 69);
+            this.Terminate_current_instance.Name = "Terminate_current_instance";
+            this.Terminate_current_instance.Size = new System.Drawing.Size(138, 36);
+            this.Terminate_current_instance.TabIndex = 1;
+            this.Terminate_current_instance.Text = "Terminate this application";
+            this.Terminate_current_instance.UseVisualStyleBackColor = true;
+            this.Terminate_current_instance.Click += new System.EventHandler(this.Terminate_current_instance_Click);
+            // 
+            // New_instance
+            // 
+            this.New_instance.AutoSize = true;
+            this.New_instance.Location = new System.Drawing.Point(318, 69);
+            this.New_instance.Name = "New_instance";
+            this.New_instance.Size = new System.Drawing.Size(138, 36);
+            this.New_instance.TabIndex = 2;
+            this.New_instance.Text = "New instance";
+            this.New_instance.UseVisualStyleBackColor = true;
+            this.New_instance.Click += new System.EventHandler(this.New_instance_Click);
+            // 
+            // Terminate_all_process
+            // 
+            this.Terminate_all_process.AutoSize = true;
+            this.Terminate_all_process.Location = new System.Drawing.Point(592, 69);
+            this.Terminate_all_process.Name = "Terminate_all_process";
+            this.Terminate_all_process.Size = new System.Drawing.Size(186, 36);
+            this.Terminate_all_process.TabIndex = 3;
+            this.Terminate_all_process.Text = "Terminate all instance of application";
+            this.Terminate_all_process.UseVisualStyleBackColor = true;
+            this.Terminate_all_process.Click += new System.EventHandler(this.Terminate_all_process_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Terminate_all_process);
+            this.Controls.Add(this.New_instance);
+            this.Controls.Add(this.Terminate_current_instance);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button Close_button;
+        private System.Windows.Forms.Button Terminate_current_instance;
+        private System.Windows.Forms.Button New_instance;
+        private System.Windows.Forms.Button Terminate_all_process;
+    }
+}
+
---
+++ test/test/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Configuration;
+using System.Runtime.InteropServices;
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        //for capturing the current process id
+        private string current_process_pid = null;
+        //to check the process or new instance of the application is triggered by button or other source
+        public static string trigger_by_button = ConfigurationManager.AppSettings["Triggerbutton"];
+        //Aplication name
+        string s_Process_Name = "test";
+       // for setting the main application to ForegroundWindow
+            [DllImport("user32.dll")]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            private static extern bool SetForegroundWindow(IntPtr hWnd);
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd,int nCmdShow);
+        const int SW_SHOW = 5;
+        public Form1()
+        {
+            //to check the process or new instance of the application is triggered by button or other source
+            Configuration config = ConfigurationManager.OpenExeConfiguration(Application.ExecutablePath);
+            Process process = Process.GetCurrentProcess();
+            current_process_pid = process.Id.ToString();
+            // if the instance is triggered by button 
+            if (trigger_by_button == "true")
+            {
+                InitializeComponent();
+                config.AppSettings.Settings["Triggerbutton"].Value = "false";
+                config.Save(ConfigurationSaveMode.Modified, true);
+                ConfigurationManager.RefreshSection("appSettings");                
+                config = null;
+            }
+            //if triggered by other means fileexplorer or cmd prompt
+            else if (trigger_by_button == "false")
+            {
+                Process[] processes = Process.GetProcessesByName(s_Process_Name);
+                if (processes.Length > 1)
+                {// to bring the main windows form process to foreground
+                            ShowWindow(processes[0].MainWindowHandle, SW_SHOW);
+                            SetForegroundWindow(processes[0].MainWindowHandle);                            
+                     //To kill this process   
+                    process.Kill();
+                }
+                else
+                {
+                    InitializeComponent();
+                }
+            }
+            else
+            {
+                InitializeComponent();
+            }
+        }
+        //using the custom button and not the windows close button
+        private void Close_button_Click(object sender, EventArgs e)
+        {
+            ShowInTaskbar = false;            
+        }
+        // to terminate the current application
+        private void Terminate_current_instance_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        // to trigger the new instance of application
+        private void New_instance_Click(object sender, EventArgs e)
+        {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(Application.ExecutablePath);
+            config.AppSettings.Settings["Triggerbutton"].Value = "true";
+            config.Save(ConfigurationSaveMode.Modified, true);
+            ConfigurationManager.RefreshSection("appSettings");
+            Process.Start(Application.ExecutablePath);
+
+            config = null;            
+        }
+        // to terminate all the process 
+        private void Terminate_all_process_Click(object sender, EventArgs e)
+        {
+            Process[] processes = Process.GetProcessesByName(s_Process_Name);
+            if (processes.Length > 0)
+            {
+                foreach (var process in processes)
+                {
+                    Process process_id = Process.GetProcessById(process.Id);
+                    if (current_process_pid != process.Id.ToString())
+                        process_id.Kill();
+                }
+            }            
+            Application.Exit();
+        }
+        //for drag and drop
+        bool mousedown ;
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mousedown = false;
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mousedown = true;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mousedown)
+            {
+                int mousex = MousePosition.X - 400;
+                int mousey = MousePosition.Y - 20;
+                this.SetDesktopLocation(mousex, mousey);
+            }
+        }
+    }
+}
